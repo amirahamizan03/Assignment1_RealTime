8798be84843007aae3bd7d167dd06ec7b78f7a71
miramizan1904 <melontri199@gmail.com>
2025-05-10 23:42:57 +0800

    Add files via upload

---
+++ Real-time/Real-time/studentinfo.java
@@ -0,0 +1,15 @@
+public class studentinfo {
+    public static void main(String[] args) {
+        String name = "Nurul Amira";
+        String matrixNo = "279388";
+        String program = "Bachelor of Information Technology";
+
+        // Display the student's info
+        System.out.println("Student Name: " + name);
+        System.out.println("Matrix No: " + matrixNo);
+        System.out.println("Program: " + program);
+
+        // Issue: Program not displaying correctly
+        // Solved: Fixed println for program display
+    }
+}
