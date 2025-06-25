ec8ebbbceb6aa0e73de351fad198e9007cf929c9
miramizan1904 <melontri199@gmail.com>
2025-06-25 22:43:05 +0800

    Delete tutorial-thread directory

--- tutorial-thread/src/main/java/week4/ThreadRunnable.java
+++
@@ -1,9 +0,0 @@
-package week4;
-
-class ThreadRunnable {
-    public static void main(String[] args){
-        Thread t = new Thread();
-        t.start();
-        System.out.println(t.getState());
-    }
-}
